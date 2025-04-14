b2f38656c8537b2959fe3a7a7057bf3f221d5632
Stephen Paul Weber <singpolyma@singpolyma.net>
2025-04-14 17:08:53 -0400

    Initial Commit

---
+++ Snikket/SnikketApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnikketApp.swift
+//  Snikket
+//
+//  Created by Stephen Weber on 2025-04-14.
+//
+
+import SwiftUI
+
+@main
+struct SnikketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SnikketTests/SnikketTests.swift
@@ -0,0 +1,16 @@
+//
+//  SnikketTests.swift
+//  SnikketTests
+//
+//  Created by Stephen Weber on 2025-04-14.
+//
+
+import Testing
+
+struct SnikketTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
